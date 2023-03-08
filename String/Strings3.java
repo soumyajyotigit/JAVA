9dfe71ccca12f56409c1aa4df07e09f20f88b616
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2023-03-09 00:57:28 +0530

    Add files via upload

--- String/Strings3.java
+++ String/Strings3.java
@@ -1,3 +1,8 @@
+import java.util.*;
 public class Strings3 {
-    
+    public static void main(String[]args){
+        String name="Soumyajyoti Karmakar";
+        String substr=name.substring(0,11);
+        System.out.print(substr);
+    }
 }
