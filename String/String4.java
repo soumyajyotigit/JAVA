2d016408792b552f8fac0b2b2a7e3c2088278e4b
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2023-03-09 02:33:28 +0530

    Add files via upload

---
+++ String/String4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class String4 {
+    public static void main(String[]args){
+        StringBuilder sb=new StringBuilder("Soumya");
+        System.out.println(sb);
+
+        sb.insert(6,'K');
+        System.out.println(sb);
+    }
+}
